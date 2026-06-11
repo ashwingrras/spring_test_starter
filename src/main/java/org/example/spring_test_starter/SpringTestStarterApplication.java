15fc39e7f38a69f1274dfd373194de8c387b76af
Ashwin <ashwanibelani10@gmail.com>
2026-06-11 17:03:38 +0530

    Initial commit

---
+++ src/main/java/org/example/spring_test_starter/SpringTestStarterApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_test_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_test_starter/SpringTestStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_test_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
